8db8395acb1c12c788b7319843a38b15c3303ac0
Weiyuan <ax61316@hotmail.com>
2017-10-02 11:30:36 +0800

    hw1

---
+++ HW1/hw1_class.hpp
@@ -0,0 +1,23 @@
+// You should try template in your code
+class list
+{
+public:
+    list();
+    ~list();
+};
+
+class poly
+{
+public:
+    poly();
+    ~poly();
+    int add(int,int);
+};
+
+class spmatrix
+{
+public:
+    spmatrix();
+    ~spmatrix();
+    int mul(int,int);
+};
