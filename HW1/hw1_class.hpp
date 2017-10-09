cc5c040817607d68492ce0d24a50dcf700948e83
wei-yuan <ax61316@hotmail.com>
2017-10-09 20:35:24 +0800

    mergesort but it needs debugging

---
+++ HW1/hw1_class.hpp
@@ -0,0 +1,108 @@
+# ifndef HW1_CLASS_HPP
+# define HW1_CLASS_HPP
+
+# include <iostream>
+# include <vector> 
+
+using namespace std;
+
+// You should try template in your code
+/**/
+class list
+{
+public:
+    list();
+    ~list();
+    int split(int,int);    
+    int compare(int,int);
+    int merge(int,int);
+};
+
+class poly
+{
+public:
+    poly();
+    ~poly();
+    int add(int,int);
+};
+
+class spmatrix
+{
+public:
+    spmatrix();
+    ~spmatrix();
+    int mul(int,int);
+};
+
+// Sorting array with random number inside
+// Odd & Even number of array size should be considered
+// try iterator instead of recursive way
+// In bottom up fashion
+
+template <typename T>
+// combime array[low, mid-1], array[mid, high]
+void _merge(T rand_arr[], int low, int mid, int high)  //vector<T> & rand_vecl: reference vector
+{
+    //array dynamic allocation 
+    T *vec_ptr = new T[high - low + 1];
+    
+    for(int i=low; i< high; i++)
+        vec_ptr[i-low] = rand_arr[i]; // i-low: make sure T[] start from zero
+
+    int i = low, j = mid + 1; // two pointer point two left & right array with number inside
+
+    for(int k = low; k<= high; k++)
+    {
+        if(i > mid) //end of left array
+        {
+            rand_arr[k] = vec_ptr[j-low]; j++;
+        }            
+        else if(j > high) //end of right array    
+        {
+            rand_arr[k] = vec_ptr[i-low]; i++;
+        }            
+        else if(vec_ptr[i] < vec_ptr[j])
+        {
+            rand_arr[k] = vec_ptr[i-low]; i++;
+        }
+        else
+        {
+            rand_arr[k] = vec_ptr[j-low]; j++;
+        }            
+    }
+    //garbage collection
+    delete [] vec_ptr;
+}
+
+template <typename T>
+void _mergesort(T rand_arr[], int low, int high)
+{
+    int mid = (low + high) / 2;
+    _mergesort(rand_arr, low, mid);
+    _mergesort(rand_arr, mid+1 , high);
+}
+
+template <typename T>
+void mergesort(T rand_arr[], int num_element)
+{
+    _mergesort(rand_arr, 0, num_element-1);
+}
+
+template <typename T>
+void mergesort_bottom_up(T rand_arr[], int num_element)
+{   
+    // interval size control
+    for(int size = 1; size < num_element; size *= 2)
+    {       
+        cout << "size: " << size << endl;             
+        // over vector iteration
+        for(int i = 0; i < num_element - size; i += size*2)
+        {
+            cout << "i: " << i << endl;            
+            if( rand_arr[ i + size - 1 ] > rand_arr[ i + size ] )
+            _merge(rand_arr, i, i+size-1, min( i+size*2-1, num_element-1) ); //arr, low, mid, high
+        }
+    }
+}
+
+# endif
--- HW1/hw1_o100442_main.cpp
+++ HW1/hw1_o100442_main.cpp
@@ -1,9 +1,32 @@
 # include <iostream>
+# include <vector> 
+# include "hw1_class.hpp"
 
-# include "hw1_class.h"
+using namespace std;
+
 
 int main()
 {
+    // using vector instead of array in C
+    //int A[5] = {-1, 5, 3, 1, 7};
+    //vector<int> vectorA(A + 1, A + 5); //vector start pos ~ end pos + 1
+    int tmpA[4] = {5, 3, 1, 7};
 
+    int length = sizeof(tmpA)/sizeof(*tmpA);
+    cout << "length: " <<  length << endl;
+    //mergesort            
+    mergesort_bottom_up(tmpA, length);
+    
+    cout << "After sorted" << endl;
+    for(int i=0; i< length; i++)
+        cout << "tmpA[" << i << "]: " << tmpA[i] << endl;
+    
+ /*   int tmpB[4] = {8, 6, 2, 4};
+    mergesort(tmpA, length);
+
+    cout << "After sorted" << endl;
+    for(int i=0; i< length; i++)
+        cout << "tmpB[" << i << "]: " << tmpB[i] << endl;    
+*/
     return 0;
 }
