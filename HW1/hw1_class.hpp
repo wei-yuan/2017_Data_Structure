c9bf176b6dc46aac0728d256b251f37f4678e5fd
wei-yuan <ax61316@hotmail.com>
2017-10-17 10:01:44 +0800

    still working on add function in class poly - 2d vector insert()

--- HW1/hw1_class.hpp
+++ HW1/hw1_class.hpp
@@ -10,11 +10,9 @@
 // Reference object and inheritance a class, avoid friend
 class List
 {
-    //private data member, allow 2-Dimensional data of vector 
-    //protected ???
-    std::vector<int> _1d_array;
-
+    //protected data member, allow 2-Dimensional data of vector 
 protected:
+    std::vector<int> _1d_array;
     std::vector< std::vector<int> > _2d_array;
 
 public:    
@@ -40,46 +38,65 @@ public:
         for(size_t row = 0; row < _2d_array.size(); row++)
             for(size_t col = 0; col < _2d_array[row].size(); col++)
                 std::cout << "list_2d_array[" << row << "][" << col << "]: " << _2d_array[row][col] <<  std::endl;
-    }
-
+    }    
     //common operation, merge two list
-    void merge()
+    void merge(std::vector< std::vector<int> > & input_2d_array_1, int row, int idx)
     {
-              
+        _2d_array[row][idx] += input_2d_array_1[row][idx];
     }
+    //TODO compare(), enum?
 };
 
 class Poly : public List //inheritance of class List
-{    
-public:
+{   
+protected:
+    int row = 1;     
+public:    
     //constructor & destructor
     Poly(std::vector< std::vector<int> > & input_2d_array)
     {
         _2d_array = input_2d_array;
     }    
     //use merge() in class List to merge two poly object
+    // input list class vector ?
     void add(std::vector< std::vector<int> > & input_2d_array)
     {
         int input_size = input_2d_array.size();
         int self_size = _2d_array.size();
+        int idx_input, idx_self;
+        idx_input = idx_self = 0;
+
+        std::vector< std::vector<int> >::iterator row_Iter; //between rows
+        std::vector<int> ::iterator col_Iter; //between col in same row
 
         for(int i=0; i < std::max(input_size, self_size); i++)
         {
-            if( input_2d_array[0][i] == _2d_array[0][i] )
+            if( input_2d_array[0][idx_input] == _2d_array[0][idx_self] )
             {
                 //same, merge
+                merge(input_2d_array, row, idx_input);
+                // increment index value
+                idx_input++; idx_self++;
             }                
-            else if ( input_2d_array[0][i] > _2d_array[0][i] )
+            else if ( input_2d_array[0][idx_input] > _2d_array[0][idx_self] ) 
             {
-                //same, merge
+                // term: input array > self
+                // only increment self
+                idx_self++;
             }
             else
             {
-               //same, merge 
+                // term: input array < self
+                // insert into self vector
+                for(row_Iter = _2d_array.begin(); row_Iter != _2d_array.end(); row++)
+                {
+                    //_2d_array.insert(row, );
+                    //input_2d_array.begin() + idx_self, input_2d_array[0][idx_input]
+                }                                     
+                // increment 
+                idx_input++;
             }
-
         }            
-
     }
 };
 
