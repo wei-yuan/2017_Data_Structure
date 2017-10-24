c810dfd57f72b9ede27838fa768da5d9f0c19c7e
wei-yuan <ax61316@hotmail.com>
2017-10-25 00:13:02 +0800

    Cannot fix the problem of returning right linked list result because of the life cycle of function

--- Linked_List/main.cpp
+++ Linked_List/main.cpp
@@ -1,48 +1,18 @@
-#include "Linked_Lisrt.hpp"
 #include <iostream>
+#include "Linked_Lisrt.hpp"
 
 using namespace std;
 
 // TODO: template version?
 
 // only for the 1st Node
-void initNode(struct Node *head, int intput_data) {
-    // New a head of list
-    Node init_Node;
-    head = &init_Node;
+void initNode(struct Node *head, int intput_data) {    
     // Assignment
     head->data = intput_data;
     head->next = NULL;
     cout << "Struct node data= " << head->data << ", Next struct node= " << head->next << endl;
 }
 
-// appending
-void appendNode(struct Node *head, int n) 
-{
-    // iterate to last node
-    while(head->next != NULL);
-    
-}
-
-void insertFront(struct Node *head, int inset_num) 
-{
-    // create new front Node
-    struct Node* new_front_Node_ptr; 
-    new_front_Node_ptr->data = inset_num;
-    new_front_Node_ptr->next = head;
-    // head pointer point to new front Node
-    head = new_front_Node_ptr; 
-}
-/*
-struct Node* searchNode(struct Node *head, int n)
-{
-
-}
-
-bool deleteNode()
-{
-}
-*/
 void display(struct Node *head) {    
     int i = 0;
     
@@ -62,13 +32,51 @@ void display(struct Node *head) {
     }    
 }
 
+// appending
+void appendNode(struct Node *head, int n) 
+{
+    // iterate to last node
+    while(head->next != NULL);
+    
+}
+
+struct Node *insertFront(struct Node *head, int insert_num) 
+{
+    // create new front Node
+    struct Node* new_front_Node_ptr;
+    Node new_front_Node;
+    new_front_Node_ptr = &new_front_Node;
+    new_front_Node_ptr->data = insert_num;
+    new_front_Node_ptr->next = head;
+    // head pointer point to new front Node
+    head = new_front_Node_ptr;
+    display(head);
+    return head;
+}
+/*
+struct Node* searchNode(struct Node *head, int n)
+{
+
+}
+
+bool deleteNode()
+{
+}
+*/
+
+
 int main() {
     // pointer to struct
-    struct Node* head;
-    struct Node* tail;
+    struct Node *head;
+    struct Node *tail;
     // create 3 node
+    // New a head of list
+    Node init_Node;
+    head = &init_Node;
     initNode(head, 1);
-    cout << "Struct node data= " << head->data << ", Next struct node= " << head->next << endl;    
+    cout << "Struct node data= " << head->data << ", Next struct node= " << head->next << endl;
+    head = insertFront(head, 0);
+
 
     // show list
     display(head);
