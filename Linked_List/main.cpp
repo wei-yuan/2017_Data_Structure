7cd5b86e068255a847a645f64fcb83a9e9b06164
Weiyuan <ax61316@hotmail.com>
2017-10-25 10:55:27 +0800

    Display has problem, still try to figure out the reason why there is loop when copy struct pointer

--- Linked_List/main.cpp
+++ Linked_List/main.cpp
@@ -6,14 +6,63 @@ using namespace std;
 // TODO: template version?
 
 // only for the 1st Node
-void initNode(struct Node *head, int intput_data) {    
+struct Node *initNode(struct Node *head, int intput_data) {    
+    // New a head of list
+    Node init_Node;
+    head = &init_Node;
     // Assignment
     head->data = intput_data;
     head->next = NULL;
-    cout << "Struct node data= " << head->data << ", Next struct node= " << head->next << endl;
+    //cout << "Struct node data= " << head->data << ", Next struct node= " << head->next << endl;
+    return head;
 }
 
-void display(struct Node *head) {    
+void display(struct Node *head) {        
+    if(head == NULL)
+    {
+        cout << "Empty List" << endl;
+        return;
+    } 
+
+    int i = 0;
+    struct Node it;
+    struct Node* it_ptr = &it;
+    
+    it_ptr->data = head->data;
+    it_ptr->next = head->next;
+
+    while (it_ptr != NULL) {              
+        cout << "Struct node[" << i << "]= " << it_ptr->data << endl;                      
+        it_ptr = it_ptr->next;
+        i++;        
+    }    
+}
+
+// appending
+void appendNode(struct Node *head, int n) 
+{
+    // iterate to last node
+    while(head->next != NULL);
+    
+}
+
+struct Node *insertFront(struct Node *head, int insert_num) 
+{    
+    // create new front Node    
+    struct Node new_front_Node;
+    struct Node *new_front_Node_ptr = &new_front_Node;
+    new_front_Node_ptr->data = insert_num;
+    new_front_Node_ptr->next = head;
+    // head pointer point to new front Node
+    head = new_front_Node_ptr;
+    //display(head);
+    return head;
+}
+
+/*
+// Similar to what it does in display
+struct Node* searchNode(struct Node *head, int n)
+{
     int i = 0;
     
     if(head != NULL){
@@ -29,57 +78,31 @@ void display(struct Node *head) {
     else
     {
         cout << "Struct node[" << i << "]= " << head->data << endl;     
-    }    
-}
-
-// appending
-void appendNode(struct Node *head, int n) 
-{
-    // iterate to last node
-    while(head->next != NULL);
-    
-}
-
-struct Node *insertFront(struct Node *head, int insert_num) 
-{
-    // create new front Node
-    struct Node* new_front_Node_ptr;
-    Node new_front_Node;
-    new_front_Node_ptr = &new_front_Node;
-    new_front_Node_ptr->data = insert_num;
-    new_front_Node_ptr->next = head;
-    // head pointer point to new front Node
-    head = new_front_Node_ptr;
-    display(head);
-    return head;
-}
-/*
-struct Node* searchNode(struct Node *head, int n)
-{
-
+    }
 }
 
 bool deleteNode()
 {
 }
-*/
 
+deleteList(struct Node **head)
+{
+
+}
+*/
 
 int main() {
     // pointer to struct
     struct Node *head;
     struct Node *tail;
-    // create 3 node
-    // New a head of list
-    Node init_Node;
-    head = &init_Node;
-    initNode(head, 1);
-    cout << "Struct node data= " << head->data << ", Next struct node= " << head->next << endl;
+    // create 3 node    
+    head = initNode(head, 1);
+    display(head);
     head = insertFront(head, 0);
-
+    cout << "Struct node data= " << head->data << ", Next struct node= " << head->next << endl;
 
     // show list
-    display(head);
+    //display(head);
 
     return 0;
 }
